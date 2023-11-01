67eb71c7945052268db0ae8ca0f854e23a5e3e41
kouji_kawamura <45913424+kouji0705@users.noreply.github.com>
2023-11-01 17:16:29 +0900

    add:

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+const initialValues = {
+  name: '',
+  email: '',
+};
+
+const validationSchema = Yup.object({
+  name: Yup.string().required('名前は必須項目です'),
+  email: Yup.string().email('有効なメールアドレスを入力してください').required('メールアドレスは必須項目です'),
+});
 
 function App() {
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: (values) => {
+      // フォームが送信されたときの処理
+      console.log(values);
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Formikサンプル</h1>
+      <form onSubmit={formik.handleSubmit}>
+        <div>
+          <label htmlFor="name">名前</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.name}
+          />
+          {formik.touched.name && formik.errors.name ? (
+            <div>{formik.errors.name}</div>
+          ) : null}
+        </div>
+
+        <div>
+          <label htmlFor="email">メールアドレス</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <div>{formik.errors.email}</div>
+          ) : null}
+        </div>
+
+        <button type="submit">送信</button>
+      </form>
     </div>
   );
 }
