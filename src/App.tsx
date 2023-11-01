72571ac558f63367a96a10da9a2e94f9876bf808
kouji_kawamura <45913424+kouji0705@users.noreply.github.com>
2023-11-01 22:50:21 +0900

    fix:

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { UserForm } from './Form/UserForm';
 import { useMyFormik } from './Form/hooks';
+import { UserFormFields } from './Form/UserFormFileds';
 
 function App() {
   // Form情報を取得
@@ -9,7 +10,7 @@ function App() {
     <div>
       <h1>Formikサンプル</h1>
       <UserForm formik={formik}>
-        <div>test</div>
+        <UserFormFields formik={formik} />
       </UserForm>
     </div>
   );
